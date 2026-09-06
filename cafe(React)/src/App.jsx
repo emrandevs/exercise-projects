859b88f3d4ee9446d0a2cccc0110d906f53748dc
emran dev <emran.shokofaki@gmail.com>
2026-09-06 15:45:20 +0330

    refacor the menu part of the cafe

--- cafe(React)/src/App.jsx
+++ cafe(React)/src/App.jsx
@@ -3,10 +3,31 @@ import Menu from "./components/Menu/Menu"
 import Footer from "./components/Footer/Footer"
 
 function App() {
+  const menuItems = [
+    {name:"espresso",price:"2"},
+    {name:"latte",price:"4"},
+    {name:"ice cream",price:"1"},
+    {name:"milk",price:"1"},
+    {name:"americano",price:"3"},
+    {name:"afogato",price:"3"},
+    {name:"flat white",price:"4"}
+  ];
   return (
     <>
       <Hero/>
-      <Menu/>
+      <div className="menu-container">
+        <div className="menu-line"></div>
+        <h2 className="menu-head">Menu</h2>
+        <div className="menu-items">
+          <Menu {...menuItems[0]}/>
+          <Menu {...menuItems[1]}/>
+          <Menu {...menuItems[2]}/>
+          <Menu {...menuItems[3]}/>
+          <Menu {...menuItems[4]}/>
+          <Menu {...menuItems[5]}/>
+          <Menu {...menuItems[6]}/>
+        </div>
+      </div>
       <Footer/>
     </>
   )
--- cafe(React)/src/components/Menu/Menu.jsx
+++ cafe(React)/src/components/Menu/Menu.jsx
@@ -1,30 +1,11 @@
-import "./Menu.css"
+import "./Menu.css";
 
-export default function Menu() {
-    function item(){
-        return (
-            <div className="item">
-                <div className="item-caption">
-                    <p className="item-name">Latte</p>
-                    <p className="item-price">3$</p>
-                </div>
-            </div>
-        )
-    }
+export default function Menu({name , price}) {
   return (
-    <div className="menu-container">
-        <div className="menu-line"></div>
-        <h2 className="menu-head">Menu</h2>
-        <div className="menu-items">
-            {item()}
-            {item()}
-            {item()}
-            {item()}
-            {item()}
-            {item()}
-            {item()}
-            {item()}
-            {item()}
+    <div className="item">
+        <div className="item-caption">
+          <p className="item-name">{name}</p>
+          <p className="item-price">{price}$</p>
         </div>
     </div>
   )
